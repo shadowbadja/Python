cb8f69f450efbae3e6886435ea4f98b2bddde89b
Steve Divine <steve@divinesecurity.net>
2020-06-10 22:48:02 -0500

    test
    test

--- hello.py
+++ hello.py
@@ -7,6 +7,6 @@ print('It is good to meet you, ' + myName)
 print('The length of your name is:')
 print(len(myName))
 # ask for their age
-print('What is your age?')
+print('How many years old are you?')
 myAge = input()
 print('You will be ' + str(int(myAge) + 1) + ' in a year.')
